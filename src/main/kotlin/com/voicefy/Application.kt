8cd2e8a2aa95492af1c87f09ac286d68aad4fe3e
Ramon Marques <140916877+ramonmarqueslima@users.noreply.github.com>
2024-01-15 21:09:36 -0300

    Initial commit

---
+++ src/main/kotlin/com/voicefy/Application.kt
@@ -0,0 +1,13 @@
+package com.voicefy
+
+import com.voicefy.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
